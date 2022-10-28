2cfb97520ea3995ef7b652ec270376fd37f74bfe
brik13ET <aleev.ibragim0909@gmail.com>
2022-10-28 15:00:46 +0400

    fill

---
+++ Network_1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Network_1
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+			this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+			this.listBox2 = new System.Windows.Forms.ListBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox3 = new System.Windows.Forms.ListBox();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+			this.splitContainer1.Panel1.SuspendLayout();
+			this.splitContainer1.Panel2.SuspendLayout();
+			this.splitContainer1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+			this.splitContainer2.Panel1.SuspendLayout();
+			this.splitContainer2.Panel2.SuspendLayout();
+			this.splitContainer2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// splitContainer1
+			// 
+			this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+			this.splitContainer1.Name = "splitContainer1";
+			this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer1.Panel1
+			// 
+			this.splitContainer1.Panel1.Controls.Add(this.button1);
+			this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+			this.splitContainer1.Panel1.Controls.Add(this.listBox3);
+			// 
+			// splitContainer1.Panel2
+			// 
+			this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+			this.splitContainer1.Size = new System.Drawing.Size(1000, 517);
+			this.splitContainer1.SplitterDistance = 259;
+			this.splitContainer1.TabIndex = 0;
+			// 
+			// splitContainer2
+			// 
+			this.splitContainer2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.splitContainer2.Location = new System.Drawing.Point(12, 3);
+			this.splitContainer2.Name = "splitContainer2";
+			this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+			// 
+			// splitContainer2.Panel1
+			// 
+			this.splitContainer2.Panel1.Controls.Add(this.listBox2);
+			// 
+			// splitContainer2.Panel2
+			// 
+			this.splitContainer2.Panel2.Controls.Add(this.listBox1);
+			this.splitContainer2.Size = new System.Drawing.Size(976, 239);
+			this.splitContainer2.SplitterDistance = 128;
+			this.splitContainer2.TabIndex = 0;
+			// 
+			// listBox2
+			// 
+			this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.listBox2.FormattingEnabled = true;
+			this.listBox2.ItemHeight = 25;
+			this.listBox2.Location = new System.Drawing.Point(0, 0);
+			this.listBox2.Name = "listBox2";
+			this.listBox2.Size = new System.Drawing.Size(976, 128);
+			this.listBox2.TabIndex = 1;
+			// 
+			// listBox1
+			// 
+			this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.ItemHeight = 25;
+			this.listBox1.Location = new System.Drawing.Point(0, 0);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(976, 107);
+			this.listBox1.TabIndex = 0;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(914, 44);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(932, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(56, 44);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Запуск";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listBox3
+			// 
+			this.listBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox3.FormattingEnabled = true;
+			this.listBox3.Location = new System.Drawing.Point(3, 70);
+			this.listBox3.Name = "listBox3";
+			this.listBox3.Size = new System.Drawing.Size(997, 173);
+			this.listBox3.TabIndex = 2;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1000, 517);
+			this.Controls.Add(this.splitContainer1);
+			this.MinimumSize = new System.Drawing.Size(800, 360);
+			this.Name = "Form1";
+			this.Text = "Алеев 6304";
+			this.splitContainer1.Panel1.ResumeLayout(false);
+			this.splitContainer1.Panel1.PerformLayout();
+			this.splitContainer1.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+			this.splitContainer1.ResumeLayout(false);
+			this.splitContainer2.Panel1.ResumeLayout(false);
+			this.splitContainer2.Panel2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+			this.splitContainer2.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.SplitContainer splitContainer1;
+		private System.Windows.Forms.SplitContainer splitContainer2;
+		private System.Windows.Forms.ListBox listBox2;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ListBox listBox3;
+	}
+}
+
---
+++ Network_1/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Network_1
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		List<string> links = new List<string>();
+		int links_delta;
+		HttpRequestMessage message;
+		System.Net.Http.HttpClient client = new System.Net.Http.HttpClient();
+
+		private async void responce_handler()
+		{
+			string doc;
+			string resp_headers = "";
+			HttpResponseMessage resp;
+			List<string> images = new List<string>();
+			while (links_delta != 0)
+			{
+				links_delta = 0;
+				if (links[0] == "/")
+					continue;
+
+				if (links[0][0] == '/' && links[0] != "/")
+				{ 
+					links[0] = "http:/" + links[0];
+					System.Uri uri = new Uri(links[0]);
+					message = new HttpRequestMessage(HttpMethod.Get, uri);
+					message.Headers.Add("Host", uri.Host);
+					message.Method = HttpMethod.Get;
+					message.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0");
+					message.Headers.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8");
+					message.Headers.Add("Accept-Language", "ru");
+					message.Headers.Add("Accept-Encoding", "");
+					links.Remove(links[0]);
+				}
+
+				try
+				{
+					resp = await client.SendAsync(message);
+				}
+				catch (SocketException e)
+				{
+					MessageBox.Show(e.Message);
+					return;
+				}
+				catch (WebException e)
+				{
+					MessageBox.Show(e.Message);
+					return;
+				}
+				catch (Exception e)
+				{
+					MessageBox.Show(e.Message, "Undefined Exception");
+					return;
+				}
+				doc = await resp.Content.ReadAsStringAsync();
+				resp_headers = resp.Headers.ToString();
+
+				foreach (String s in doc.Replace("><", ">\n<").Replace("\r", "").Split('\n'))
+				{
+					Match match;
+					if (s.Contains("<img"))
+						match = null;
+					match = Regex.Match(s, @"(<a[\s]+.*href=[""']([a-zA-Z.-/:]+\.(bmp|jpeg|png|jpg|svg|webp))[""'].*>)");
+					if (match.Success)
+					{
+						images.Add(match.Groups[2].Value);
+					}
+					match = Regex.Match(s, @"(<a[\s]+.*href=[""']([a-zA-Z.-/:]+)[""'].*>)");
+					if (match.Success)
+					{
+
+						if (!links.Contains(match.Groups[2].Value))
+						{
+							links.Add(match.Groups[2].Value);
+							links_delta++;
+						}
+					}
+					match = Regex.Match(s, @"(<img[\s]+.*src=[""'](.+)[""']\s?.*>)");
+					if (match.Success)
+					{
+						images.Add(match.Groups[2].Value);
+					}
+				}
+			}
+			this.Invoke
+			((MethodInvoker)delegate {
+				this.listBox1.Items.Clear();
+				this.listBox2.Items.Clear();
+				this.listBox3.Items.Clear();
+				foreach (string h in resp_headers.Split('\n') )
+					this.listBox3.Items.Add(h);
+				foreach (string img in images)
+					this.listBox2.Items.Add(img);
+				foreach (var val in links)
+					this.listBox1.Items.Add(val);
+			});
+		}
+
+		private async void button1_Click(object sender, EventArgs e)
+		{
+			links.Add(textBox1.Text);
+			links_delta = 1;
+			await Task.Run(responce_handler);
+		}
+	}
+}

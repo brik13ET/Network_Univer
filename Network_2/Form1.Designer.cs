5d206c86e8982fe2692ef3a500462476fc0553e3
brik13ET <aleev.ibragim0909@gmail.com>
2022-11-09 16:16:35 +0400

    Synced Lab 2

---
+++ Network_2/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Network_2
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+			System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.treeView2 = new System.Windows.Forms.TreeView();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.FileList = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.SizeList = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.Summary = new System.Windows.Forms.DataGridViewTextBoxColumn();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox1.Location = new System.Drawing.Point(12, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(529, 29);
+			this.textBox1.TabIndex = 1;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// treeView2
+			// 
+			this.treeView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.treeView2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+			this.treeView2.Location = new System.Drawing.Point(6, 6);
+			this.treeView2.Name = "treeView2";
+			this.treeView2.Size = new System.Drawing.Size(196, 333);
+			this.treeView2.TabIndex = 4;
+			// 
+			// listBox1
+			// 
+			this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.IntegralHeight = false;
+			this.listBox1.ItemHeight = 24;
+			this.listBox1.Location = new System.Drawing.Point(208, 6);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(378, 333);
+			this.listBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.Location = new System.Drawing.Point(547, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(62, 29);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "GO";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+			this.tabControl1.Location = new System.Drawing.Point(12, 47);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(597, 382);
+			this.tabControl1.TabIndex = 7;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.treeView2);
+			this.tabPage1.Controls.Add(this.listBox1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 33);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(589, 345);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.dataGridView1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 33);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(589, 345);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.AllowUserToAddRows = false;
+			this.dataGridView1.AllowUserToDeleteRows = false;
+			this.dataGridView1.AllowUserToOrderColumns = true;
+			dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+			dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+			dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+			dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+			dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+			dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+			dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+			this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+			this.dataGridView1.ColumnHeadersHeight = 26;
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+			this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileList,
+            this.SizeList,
+            this.Summary});
+			this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+			this.dataGridView1.GridColor = System.Drawing.SystemColors.Control;
+			this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.ReadOnly = true;
+			this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+			dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+			this.dataGridView1.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+			this.dataGridView1.RowTemplate.Height = 26;
+			this.dataGridView1.RowTemplate.ReadOnly = true;
+			this.dataGridView1.Size = new System.Drawing.Size(583, 339);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// FileList
+			// 
+			dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.TopLeft;
+			this.FileList.DefaultCellStyle = dataGridViewCellStyle3;
+			this.FileList.HeaderText = "Файлы";
+			this.FileList.Name = "FileList";
+			this.FileList.ReadOnly = true;
+			this.FileList.Width = 200;
+			// 
+			// SizeList
+			// 
+			this.SizeList.HeaderText = "Exten";
+			this.SizeList.Name = "SizeList";
+			this.SizeList.ReadOnly = true;
+			this.SizeList.Width = 200;
+			// 
+			// Summary
+			// 
+			this.Summary.HeaderText = "Size(bytes)";
+			this.Summary.Name = "Summary";
+			this.Summary.ReadOnly = true;
+			this.Summary.Width = 200;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(624, 441);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.MinimumSize = new System.Drawing.Size(640, 480);
+			this.Name = "Form1";
+			this.Text = "Network FTP";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TreeView treeView2;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridViewTextBoxColumn FileList;
+		private System.Windows.Forms.DataGridViewTextBoxColumn SizeList;
+		private System.Windows.Forms.DataGridViewTextBoxColumn Summary;
+	}
+}
+
---
+++ Network_2/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Network_2
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		List<string> files = new List<string>();
+		List<string> dirs = new List<string>();
+		string host = "91.222.128.11", login = "testftp_guest", pass = "12345";
+		private void doReq()
+		{
+			FtpWebRequest req = (FtpWebRequest)WebRequest.Create("ftp://" + host);
+			req.Credentials = new NetworkCredential(login, pass);
+			req.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+
+			var root = new TreeNode(host);
+
+			var c = (new StreamReader(req.GetResponse().GetResponseStream())).ReadToEnd().Replace("\r", "").Split('\n');
+			Match m;
+			foreach (var i in c)
+			{
+				m = Regex.Match(i, @"^d[rwx-]{3,9}([\s\t]+\d+){4}\s\w{3}\s\d\d\s+[\d:]+\s(.*)$");
+				if (m.Success)
+				{
+					var _ = new TreeNode(m.Groups[2].Value);
+					_.Tag = m.Groups[2].Value;
+					root.Nodes.Add(_);
+					dirs.Add("/" + m.Groups[2].Value);
+				}
+				m = Regex.Match(i, @"^[rwx-]{3,9}([\s\t]+\d+){4}\s\w{3}\s\d\d\s+[\d:]+\s(.*)$");
+				if (m.Success)
+					files.Add(m.Groups[2].Value);
+			}
+			string pwd;
+
+			for (int i = 0; i  < dirs.Count - 1; i ++)
+			{
+				if (dirs[i].Length == 0)
+				{
+					dirs.Remove(dirs[i]);
+					i--;
+					continue;
+				}
+
+				pwd = dirs[i];
+				req = (FtpWebRequest)WebRequest.Create("ftp://" + host + pwd);
+				req.Credentials = new NetworkCredential(login, pass);
+				req.Method = WebRequestMethods.Ftp.ListDirectoryDetails;
+				var tmp =
+					(
+						new StreamReader(
+							req
+								.GetResponse()
+								.GetResponseStream()
+						)
+					)
+						.ReadToEnd()
+						.Replace("\r", "")
+						.Split('\n');
+				foreach (var item in tmp)
+				{
+					m = Regex.Match(item, @"^d[rwx-]{3,9}([\s\t]+\d+){4}\s\w{3}\s\d\d\s+[\d:]+\s(.*)$");
+					if (m.Success)
+					{
+						dirs.Add(pwd + "/" + m.Groups[2].Value);
+						TreeNode n = root;
+						var s_pwd = pwd.Substring(1).Split('/');
+						for (int j = 0; n != null && j < s_pwd.Length; j++)
+						{
+							n = n.FirstNode;
+							while (n != null && n.Text != s_pwd[j])
+								n = n.NextNode;
+						}
+						var _ = new TreeNode(m.Groups[2].Value);
+						_.Tag = m.Groups[2].Value;
+						n.Nodes.Add(_);
+					}
+					m = Regex.Match(item, @"^-[rwx-]{3,9}([\s\t]+\d+){4}\s\w{3}\s\d\d\s+[\d:]+\s(.*)$");
+					if (m.Success)
+						files.Add(pwd + "/" + m.Groups[2].Value);
+				}
+			}
+
+			this.Invoke((MethodInvoker)delegate
+			{
+				Dictionary<string, int> cnt = new Dictionary<string, int>();
+				dataGridView1.Rows.Clear();
+				int sum = 0;
+				foreach (string f in files)
+				{
+					req = (FtpWebRequest)WebRequest.Create("ftp://" + host + f);
+					req.Credentials = new NetworkCredential(login, pass);
+					req.Method = WebRequestMethods.Ftp.GetFileSize;
+					var f_siz = req.GetResponse().ContentLength;
+					m = Regex.Match(f, @"^/(.+/)*[\w\s.-]+(?<extt>(\.[a-z]+){1,2})$");
+					if (!m.Success)
+						break;
+					var ext = m.Groups["extt"].Value;
+					if (cnt.ContainsKey(ext))
+						cnt[ext]+=(int)f_siz;
+					else
+						cnt.Add(ext, (int)f_siz);
+					sum += (int)f_siz;
+					dataGridView1.Rows.Add(f, ext, f_siz);
+				}
+				dataGridView1.Rows.Add("", "", "");
+				foreach (KeyValuePair<string,int> pa in cnt)
+				{
+					dataGridView1.Rows.Add("*", pa.Key, pa.Value);
+				}
+				dataGridView1.Rows.Add("*", "*", sum);
+
+				foreach (var item in root.Nodes)
+					treeView2.Nodes.Add(item as TreeNode);
+				listBox1.Items.AddRange(files.ToArray());
+				files.Clear();
+				dirs.Clear();
+				button1.Enabled = true;
+			});
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Task t = new Task(doReq);
+			t.Start();
+			if (sender is Button)
+				(sender as Button).Enabled = false;
+		}
+	}
+}
